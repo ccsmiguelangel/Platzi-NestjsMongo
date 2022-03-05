de6480bb5656001e2043d5d82e104dc5c4c80894
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2022-03-05 12:04:37 -0400

    :heavy_plus_sign: Añadido codigo en github

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,16 +2,20 @@ import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import config from './config';
 
+import { Db } from 'mongodb'; // 👈 Import DB Type
+
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
-    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+    @Inject('MONGO') private database: Db,
+    @Inject(config.KEY) private configService: ConfigType,
   ) {}
   getHello(): string {
     const apiKey = this.configService.apiKey;
     const name = this.configService.database.name;
     return `Hello World! ${apiKey} ${name}`;
   }
+  getTasks() { }  // 👈 Create new method
 }
