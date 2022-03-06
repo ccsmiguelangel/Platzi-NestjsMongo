2ef4dd8d0487211d954203f702213aa273ceb7c3
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2022-03-06 10:20:39 -0400

    :heavy_plus_sign: Agregado código de lección 7 y actualizado las dependencias
    Lección 7

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import config from './config';
-
 import { Db } from 'mongodb'; // 👈 Import DB Type
 
 @Injectable()
@@ -10,12 +9,14 @@ export class AppService {
     // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
     @Inject('MONGO') private database: Db,
-    @Inject(config.KEY) private configService: ConfigType,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
+
   getHello(): string {
     const apiKey = this.configService.apiKey;
     const name = this.configService.database.name;
     return `Hello World! ${apiKey} ${name}`;
   }
-  getTasks() { }  // 👈 Create new method
+  getTasks() { 
+  }  // 👈 Create new method
 }
